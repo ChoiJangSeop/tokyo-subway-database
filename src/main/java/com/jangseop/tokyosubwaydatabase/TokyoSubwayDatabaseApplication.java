6f92e2a8c9584049142a7a5083c727d1a97b449a
jangseop <kgh010529@naver.com>
2023-07-25 17:59:46 +0900

    first commit

---
+++ src/main/java/com/jangseop/tokyosubwaydatabase/TokyoSubwayDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.jangseop.tokyosubwaydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokyoSubwayDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokyoSubwayDatabaseApplication.class, args);
+	}
+
+}
