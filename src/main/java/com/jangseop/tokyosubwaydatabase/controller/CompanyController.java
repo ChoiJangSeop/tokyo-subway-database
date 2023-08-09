4c800a57943e58875fb46b0ce878e384865722a8
jangseop <kgh010529@naver.com>
2023-08-10 03:25:02 +0900

    implement post method of company

--- src/main/java/com/jangseop/tokyosubwaydatabase/controller/CompanyController.java
+++ src/main/java/com/jangseop/tokyosubwaydatabase/controller/CompanyController.java
@@ -1,12 +1,20 @@
 package com.jangseop.tokyosubwaydatabase.controller;
 
+import com.jangseop.tokyosubwaydatabase.controller.dto.CompanyCreateRequest;
+import com.jangseop.tokyosubwaydatabase.controller.dto.CompanyResponse;
+import com.jangseop.tokyosubwaydatabase.domain.Company;
 import com.jangseop.tokyosubwaydatabase.service.CompanyService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.jangseop.tokyosubwaydatabase.service.CompanyService.*;
 
 @AllArgsConstructor
 @RestController
@@ -16,4 +24,12 @@ public class CompanyController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final CompanyService companyService;
+
+    @PostMapping("/company")
+    public ResponseEntity<CompanyResponse> newOne(@RequestBody CompanyCreateRequest request) {
+        CompanyCreateDto dto = CompanyCreateDto.of(request.name());
+        Company company = companyService.create(dto);
+
+        return new ResponseEntity<>(CompanyResponse.of(company), HttpStatus.OK);
+    }
 }
---
+++ src/main/java/com/jangseop/tokyosubwaydatabase/controller/dto/CompanyResponse.java
@@ -0,0 +1,10 @@
+package com.jangseop.tokyosubwaydatabase.controller.dto;
+
+import com.jangseop.tokyosubwaydatabase.domain.Company;
+
+public record CompanyResponse(Long id, String name) {
+
+    public static CompanyResponse of(Company company) {
+        return new CompanyResponse(company.id(), company.name());
+    }
+}
